3de40e3822fe3e34d10e04143def58edfb4a9907
piyushgith <piyushprasad2007@gmail.com>
2020-05-06 23:53:03 +0530

    fixed data insertion in mongodb. You need to create db and collection

--- src/main/java/com/example/reactive/mongodb/ReactiveMongodbExampleApplication.java
+++ src/main/java/com/example/reactive/mongodb/ReactiveMongodbExampleApplication.java
@@ -1,44 +1,13 @@
 package com.example.reactive.mongodb;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-
-import com.example.reactive.mongodb.model.Employee;
-import com.example.reactive.mongodb.repository.EmployeeRepository;
 
 @SpringBootApplication
-@EnableReactiveMongoRepositories
 public class ReactiveMongodbExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveMongodbExampleApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner init(EmployeeRepository employeeRepository) {
-		return init -> {
-
-			List<Employee> list = new ArrayList<>();
-			list.add(new Employee(UUID.randomUUID().toString(), "Ram", new Date()));
-			list.add(new Employee(UUID.randomUUID().toString(), "Sita", new Date()));
-			list.add(new Employee(UUID.randomUUID().toString(), "Luxman", new Date()));
-			list.add(new Employee(UUID.randomUUID().toString(), "Hanuman", new Date()));
-
-			// employeeRepository.saveAll(list);
-			list.forEach(x -> {
-				System.out.println(x.toString());
-				employeeRepository.save(x);
-			});
-		};
-
 	}
 
 }
---
+++ src/main/java/com/example/reactive/mongodb/config/MongoReactiveConfig.java
@@ -0,0 +1,59 @@
+package com.example.reactive.mongodb.config;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+import com.example.reactive.mongodb.model.Employee;
+import com.example.reactive.mongodb.repository.EmployeeRepository;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+
+@Configuration
+@EnableReactiveMongoRepositories(basePackageClasses = EmployeeRepository.class)
+public class MongoReactiveConfig extends AbstractReactiveMongoConfiguration {
+
+	@Override
+	public MongoClient reactiveMongoClient() {
+		return MongoClients.create();
+	}
+
+	@Override
+	protected String getDatabaseName() {
+		return "test";
+	}
+
+	@Bean
+	public ReactiveMongoTemplate reactiveMongoTemplate() {
+		return new ReactiveMongoTemplate(reactiveMongoClient(), getDatabaseName());
+	}
+
+	@Bean
+	CommandLineRunner init(EmployeeRepository employeeRepository) {
+		return init -> {
+			//employeeRepository.deleteAll();
+			
+			List<Employee> list = new ArrayList<>();
+			list.add(new Employee(UUID.randomUUID().toString(), "Ram", new Date()));
+			list.add(new Employee(UUID.randomUUID().toString(), "Sita", new Date()));
+			list.add(new Employee(UUID.randomUUID().toString(), "Luxman", new Date()));
+			list.add(new Employee(UUID.randomUUID().toString(), "Hanuman", new Date()));
+
+			// employeeRepository.saveAll(list);
+			list.forEach(x -> {
+				System.out.println(x.toString());
+				employeeRepository.save(x).subscribe();
+			});
+		};
+
+	}
+
+}
