bb3f0cd146cac191c00de77b329a13e19a99e8c5
piyushgith <piyushprasad2007@gmail.com>
2020-05-19 23:40:57 +0530

    Updated JWT creation from String to KEY interface

--- src/main/java/com/example/reactive/mongodb/security/jwt/TokenProvider.java
+++ src/main/java/com/example/reactive/mongodb/security/jwt/TokenProvider.java
@@ -1,14 +1,7 @@
 package com.example.reactive.mongodb.security.jwt;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.Collection;
-import java.util.Date;
-import java.util.stream.Collectors;
-
-import javax.annotation.PostConstruct;
-
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -20,34 +13,37 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.SignatureException;
-import io.jsonwebtoken.UnsupportedJwtException;
+import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
 public class TokenProvider {
 
 	private final Logger log = LoggerFactory.getLogger(TokenProvider.class);
 
-	private static final String SALT_KEY = "JpxM4e858rc673syopdZnMFb*ExedfsdtyujiuuldgfsfagdfhgfjhgjqJtUc0HJ_iOxu~jiSYu+yPdPw93OBBjF";
-	private static final int TOKEN_VALIDITY = 1800; // Value in second
-
+	private static final String SECRETE_KEY = "JpxM4e858rc673syopdZnMFb*ExedfsdtyujiuuldgfsfagdfhgfjhgjqJtUc0HJ_iOxu~jiSYu+yPdPw93OBBjF";
 	private static final String AUTHORITIES_KEY = "auth";
-
-	private final Base64.Encoder encoder = Base64.getEncoder();
-
-	private String secretKey;
-
+	private final int TOKEN_VALIDITY = 300; // Value in second
 	private long tokenValidityInMilliseconds;
+
+	private Key myKey;
 
 	@PostConstruct
 	public void init() {
-		this.secretKey = encoder.encodeToString(SALT_KEY.getBytes(StandardCharsets.UTF_8));
+		//Multiple Ways to do it
+		//A Base64-encoded string: ==>
+		//SecretKey key2 = Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRATE_KEY));
+		//A Base64URL-encoded string: ==>
+		//SecretKey key3 = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(SECRATE_KEY));
+		//A raw (non-encoded) string (e.g. a password String): ==>
+		//SecretKey key4 = Keys.hmacShaKeyFor(SECRATE_KEY.getBytes(StandardCharsets.UTF_8));
 
+		myKey= Keys.hmacShaKeyFor(SECRETE_KEY.getBytes(StandardCharsets.UTF_8));
 		this.tokenValidityInMilliseconds = 1000 * TOKEN_VALIDITY;
 	}
 
@@ -57,16 +53,17 @@ public class TokenProvider {
 
 		long now = (new Date()).getTime();
 		Date validity = new Date(now + this.tokenValidityInMilliseconds);
-
-		return Jwts.builder().setSubject(authentication.getName()).claim(AUTHORITIES_KEY, authorities)
-				.signWith(SignatureAlgorithm.HS512, secretKey).setExpiration(validity).compact();
+		//creating JWT
+		return Jwts.builder().setSubject(authentication.getName()).claim(AUTHORITIES_KEY,authorities)
+				.signWith(myKey,SignatureAlgorithm.HS512).setExpiration(validity).compact();
 	}
 
 	public Authentication getAuthentication(String token) {
 		if (StringUtils.isEmpty(token) || !validateToken(token)) {
 			throw new BadCredentialsException("Invalid token");
 		}
-		Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+		//We should use Interface Key implementations not simple Strings
+		Claims claims= Jwts.parserBuilder().setSigningKey(myKey).build().parseClaimsJws(token).getBody();
 
 		Collection<? extends GrantedAuthority> authorities = Arrays
 				.stream(claims.get(AUTHORITIES_KEY).toString().split(",")).map(SimpleGrantedAuthority::new)
@@ -79,7 +76,7 @@ public class TokenProvider {
 
 	public boolean validateToken(String authToken) {
 		try {
-			Jwts.parser().setSigningKey(secretKey).parseClaimsJws(authToken);
+			Jwts.parserBuilder().setSigningKey(myKey).build().parseClaimsJws(authToken);
 			return true;
 		} catch (SignatureException e) {
 			log.info("Invalid JWT signature.");
