5ae670f4c0b2bf68218ffdebbc8d19414f5a2ca8
piyushgith <piyushprasad2007@gmail.com>
2020-05-27 21:06:17 +0530

    Added MDC compatible with WebFlux Now

--- src/main/java/com/example/reactive/mongodb/security/CorrelationFilter.java
+++ src/main/java/com/example/reactive/mongodb/security/CorrelationFilter.java
@@ -1,5 +1,7 @@
 package com.example.reactive.mongodb.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -11,11 +13,19 @@ import java.util.UUID;
 
 @Component
 public class CorrelationFilter implements WebFilter {
-//correlationId
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        //To make MDC work in webflux & No Need of Threadcontext or ThreadLocal
+        //Example https://github.com/archie-swif/webflux-mdc/blob/master/src/main/java/com/example/webfluxmdc/WebfluxContextMdcApplication.java
+        //You must have logback.xml and MdcContextLifter & MdcContextLifterConfiguration
+        //You can do some operations before return as well like calling methods
+        logger.info("List of Incoming Headers: "+exchange.getRequest().getHeaders());
+        logger.info("Ip Address: "+exchange.getRequest().getRemoteAddress().toString());
+
         return chain.filter(exchange)
-                .map(s->{System.out.println("List of Incoming Headers: "+exchange.getRequest().getHeaders());  return s; })
+                 //it can accept up to 5 key pair
                 .subscriberContext(Context.of("correlationId", "[correlationId: "+getCorrelationId(exchange)+"]", "ipAddress","[ipAddress: "+ getIpAddress(exchange)+"]"));
     }
 
